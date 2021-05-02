86b1cb5b0f36760127ca8ded75c778d347f63ed6
Willie Roberts <diagoza@me.com>
2021-05-02 13:21:21 +0200

    Added CLI project and Tests

---
+++ Senselessly.Foolish.ModTrakt.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Senselessly.Foolish.ModTrakt.Client
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
