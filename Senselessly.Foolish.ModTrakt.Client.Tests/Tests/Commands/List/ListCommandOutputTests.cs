9afca5177dcd5905ecdeb27c20d74714b2d3705d
Willie Roberts <diagoza@me.com>
2021-05-12 01:45:05 +0200

    Adding test for list command line

---
+++ Senselessly.Foolish.ModTrakt.Client.Tests/Tests/Commands/List/ListCommandOutputTests.cs
@@ -0,0 +1,48 @@
+namespace Senselessly.Foolish.ModTrakt.Client.Tests.Tests.Commands.List
+{
+    using System.CommandLine;
+    using System.CommandLine.IO;
+    using System.IO.Abstractions.TestingHelpers;
+    using Bethesda.Archives.Services;
+    using Client.Commands;
+    using Client.Commands.List;
+    using FluentAssertions;
+    using Xunit;
+
+    [Collection("ProgramListOutput")]
+    [Trait(name: "Category", value: "ProgramTests")]
+    public class ListCommandOutputTests
+    {
+        private readonly RootCommandBuilder _builder =
+            new RootCommandBuilder(new ListCommandBuilder(new PluginLocatorService(new MockFileSystem())));
+
+        private readonly IConsole _console = new TestConsole();
+
+        [Fact]
+        public async void Program_Arg_Alias_Li_Returns_Help()
+        {
+            var expected = $"list*{Properties.Resources.Command_List_Description}*";
+            var rootCommand = _builder.BuildCommand();
+            await rootCommand.InvokeAsync(commandLine: "li", console: _console);
+            _console.Out.ToString().Should().Match(expected);
+        }
+
+        [Fact]
+        public async void Program_Arg_Alias_List_Returns_Help()
+        {
+            var expected = $"list*{Properties.Resources.Command_List_Description}*";
+            var rootCommand = _builder.BuildCommand();
+            await rootCommand.InvokeAsync(commandLine: "list", console: _console);
+            _console.Out.ToString().Should().Match(expected);
+        }
+
+        [Fact]
+        public async void Program_Arg_List_Path_Invalid_Returns_Error()
+        {
+            const string expected = "Directory does not exist: C:\\Mods\\\r\n\r\n";
+            var rootCommand = _builder.BuildCommand();
+            await rootCommand.InvokeAsync(commandLine: "list C:\\Mods\\", console: _console);
+            _console.Error.ToString().Should().Match(expected);
+        }
+    }
+}
--- Senselessly.Foolish.ModTrakt.Client.Tests/Tests/ProgramOutputTests.cs
+++ Senselessly.Foolish.ModTrakt.Client.Tests/Tests/ProgramOutputTests.cs
@@ -3,6 +3,7 @@ namespace Senselessly.Foolish.ModTrakt.Client.Tests.Tests
     using System.CommandLine;
     using System.CommandLine.IO;
     using System.IO.Abstractions.TestingHelpers;
+    using System.Reflection;
     using Bethesda.Archives.Services;
     using Client.Commands;
     using Client.Commands.List;
@@ -13,18 +14,48 @@ namespace Senselessly.Foolish.ModTrakt.Client.Tests.Tests
     [Trait(name: "Category", value: "ProgramTests")]
     public class ProgramOutputTests
     {
+        private static readonly string Version = (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly())
+                                                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                               ?.InformationalVersion;
+
         private readonly RootCommandBuilder _builder =
             new RootCommandBuilder(new ListCommandBuilder(new PluginLocatorService(new MockFileSystem())));
 
         private readonly IConsole _console = new TestConsole();
 
         [Fact]
-        public async void Program_Returns_No_Args_Help_Text()
+        public async void Program_Arg_H_Returns_Help()
+        {
+            const string expected = "*Options:*--version*Show version information*";
+            var rootCommand = _builder.BuildCommand();
+            await rootCommand.InvokeAsync(commandLine: "-h", console: _console);
+            _console.Out.ToString().Should().Match(expected);
+        }
+
+        [Fact]
+        public async void Program_Arg_Help_Returns_Help()
+        {
+            const string expected = "*Options:*--version*Show version information*";
+            var rootCommand = _builder.BuildCommand();
+            await rootCommand.InvokeAsync(commandLine: "--help", console: _console);
+            _console.Out.ToString().Should().Match(expected);
+        }
+
+        [Fact]
+        public async void Program_Arg_None_Returns_Help()
         {
             const string expected = "*Options:*--version*Show version information*";
             var rootCommand = _builder.BuildCommand();
             await rootCommand.InvokeAsync(commandLine: "", console: _console);
             _console.Out.ToString().Should().Match(expected);
+        }
+
+        [Fact]
+        public async void Program_Arg_Version_Returns_Version()
+        {
+            var rootCommand = _builder.BuildCommand();
+            await rootCommand.InvokeAsync(commandLine: "--version", console: _console);
+            _console.Out.ToString().Should().Match($"{Version}\r\n");
         }
     }
 }
