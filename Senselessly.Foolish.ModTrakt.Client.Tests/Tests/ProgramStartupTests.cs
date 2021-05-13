fb106d2c720997df8385ca5efc1440a231eb8007
Willie Roberts <diagoza@me.com>
2021-05-13 11:49:58 +0200

    Fixed code smells in test project

--- Senselessly.Foolish.ModTrakt.Client.Tests/Tests/ProgramStartupTests.cs
+++ Senselessly.Foolish.ModTrakt.Client.Tests/Tests/ProgramStartupTests.cs
@@ -1,5 +1,6 @@
 namespace Senselessly.Foolish.ModTrakt.Client.Tests.Tests
 {
+    using System.Threading.Tasks;
     using FluentAssertions;
     using Helpers;
     using Xunit;
@@ -13,7 +14,7 @@ namespace Senselessly.Foolish.ModTrakt.Client.Tests.Tests
         public ProgramStartupTests(ProgramFixture program) => _program = program;
 
         [Fact]
-        public async void Program_Startup_No_Args_Return_1()
+        public async Task Program_Startup_No_Args_Return_1()
         {
             const int expected = 1;
             var result = await Program.Main();
@@ -21,7 +22,7 @@ namespace Senselessly.Foolish.ModTrakt.Client.Tests.Tests
         }
 
         [Fact]
-        public async void Program_Startup_No_Services_Return_255()
+        public async Task Program_Startup_No_Services_Return_255()
         {
             const int expected = 255;
             var result = await _program.Run();
